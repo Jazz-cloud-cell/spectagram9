3bd55c5c80b7d7e9021ac43af9e28d314b5904de
Jazz-cloud-cell <82362155+Jazz-cloud-cell@users.noreply.github.com>
2022-07-08 14:51:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBfyz4uEQQfMtLMqi_-UGO4ucDnUudrlpA",
+    authDomain: "spectagram-a89cf.firebaseapp.com",
+    databaseURL: "https://spectagram-a89cf-default-rtdb.firebaseio.com",
+    projectId: "spectagram-a89cf",
+    storageBucket: "spectagram-a89cf.appspot.com",
+    messagingSenderId: "337752148967",
+    appId: "1:337752148967:web:3b65c022c8e0142a488b42"
+  };
